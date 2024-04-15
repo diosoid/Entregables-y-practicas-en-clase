ee2316796c6919687b12528d1739dcd7b7dcaeb8
Pablo <reta_pablo_@hotmail.com>
2024-04-15 19:39:25 -0300

    Ultimos cambios

--- Desafios entregables/Primer Desafio/productManager.js
+++ Desafios entregables/Primer Desafio/productManager.js
@@ -3,10 +3,19 @@ class ProductManager {
         this.products = [];
     } 
 
-    addProduct (title, description, price, thumbnail, stock) {
+    addProduct (title, description, price, thumbnail, code, stock) {
+
+        if (!title || !description || !price || !thumbnail || !code || !stock){
+            return console.log('Todos los campos son obligatorios')
+        }
+
+        const checkCode = this.products.find((e) => e.code === code);
+        if (checkCode) {
+        return  console.log ("El articulo ya fue ingresado, por favor ingrese un producto diferente");
+        }
 
         const product = {
-            code: this.#getMaxcode() + 1 ,
+            id: this.#getMaxid() + 1 ,
             title,
             description,
             price,
@@ -15,30 +24,46 @@ class ProductManager {
             stock
         }
         this.products.push(product);
-    };    
+        
+        
 
-    #getMaxcode(){
-        let maxCode = 0;
+    }     
+
+    #getMaxid(){
+        let maxId = 0;
         this.products.map((product) => { 
-        if (product.code > maxCode) maxCode = product.code;
+        if (product.id > maxId) maxId = product.id;
         });
-        return maxCode;
+        return maxId;
     }
         
     getProduct () {
         return this.products;   
     }
 
-    getProductByCode () {
-        return this.products.find(product => product.code === code);        
+    getProductByid (id) {
+        if (id) {
+            return this.products.find(product => product.id === id);                   
+        }
+        return console.log("Product not found")
     };
 }
 
 const productManager = new ProductManager()
 
-productManager.addProduct('Coca Cola', 'Sin Azucar', 3000, "https://coca-colaentucasa.com/wp-content/uploads/2023/06/2891-COCA-COLA-ZERO-PET-500-ml-PACK-x6-2.jpg", 20 )
-productManager.addProduct('Sprite', 'Original', 2800, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg", 30 )
+productManager.addProduct('Sprite', 'Original', 2800, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",1 ,30 )
+productManager.addProduct('Fanta', 'Edicion Loolapaloza', 3400, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",2 , 50 )
+productManager.addProduct('Fanta', 'Edicion Loolapaloza', 3400, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",2 , 50 ) // CODIGO REPETIDO
+productManager.addProduct( 'Sin Azucar', 3000, "https://coca-colaentucasa.com/wp-content/uploads/2023/06/2891-COCA-COLA-ZERO-PET-500-ml-PACK-x6-2.jpg", 3, 20 ) //CAMPO INCOMPLETO
 
+// Productos en el arreglo
+console.log('Los productos en el carrito son:')
 console.log(productManager.getProduct());
+
+// Busqueda por ID
+console.log('El producto buscado es:')
+console.log(productManager.getProductByid(2));
+console.log(productManager.getProductByid(6));
+
 
 
