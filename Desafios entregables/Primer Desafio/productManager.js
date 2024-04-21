9e19dd20b9eee4d28ee45816340cf327088353e8
Pablo <reta_pablo_@hotmail.com>
2024-04-21 17:58:16 -0300

    Aplicando el update y completando el resto de las funciones.

--- Desafios entregables/Primer Desafio/productManager.js
+++ Desafios entregables/Primer Desafio/productManager.js
@@ -5,35 +5,33 @@ const fs = require('fs');
 class ProductManager {   
     constructor (path) {
         this.path = path;
-        //this.products = [];
+        this.products = [];
     } 
 
     async getProducts () {
         try {
             if(fs.existsSync(this.path)){
                 const products = await fs.promises.readFile(this.path, 'utf8');
-                return JSON.parse(products)
+                this.products = JSON.parse(products)
+                return this.products
             } else return []
             
         } catch (error) {
             console.log(error)          
         }
-        // return this.products;   // Codigo anterior...
+        
     }
     
 
-    async addProduct (title , description ,price ,thumbnail , code ,stock ) {//Aca iba prod 
+    async addProduct (title , description ,price ,thumbnail , code ,stock ) {
         try {
-            const products = await this.getProducts()
-            products.push(title , description ,price ,thumbnail , code ,stock) // Aca tmb iba prod
-            await fs.promises.writeFile(this.path, JSON.stringify(products), 'utf8');
-
-            if (!title || !description || !price || !thumbnail || !code || !stock){
-                return console.log('Todos los campos son obligatorios')
-            }
+            await this.getProducts()
             const checkCode = await this.products.find((e) => e.code === code);
             if (checkCode) {
             return  console.log ("El articulo ya fue ingresado, por favor ingrese un producto diferente");
+            }
+            if (!title || !description || !price || !thumbnail || !code || !stock){
+                return console.log('Todos los campos son obligatorios')
             }
             const product = {
                 id: this.#getMaxid() + 1 ,
@@ -45,6 +43,9 @@ class ProductManager {
                 stock
             }
             this.products.push(product);       
+            
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products), 'utf8');
+
             
         } catch (error) {
             console.log(error)
@@ -65,45 +66,77 @@ class ProductManager {
         
 
     
-    // getProductByid (id) {
+    async getProductByid (id) {
 
-    //     const  getProdByID = this.products.find((e) => e.id === id);
-
-    //     if (getProdByID) {
-    //         return getProdByID;
-    //     }
-    //     return  console.log ("Product not found");
+        const products = await fs.promises.readFile(this.path, 'utf8');
+        this.products = JSON.parse(products)
+        
+        const  getProdByID = this.products.find((e) => e.id === id);
+        
+        if (getProdByID) {
+            return getProdByID;
+        }
+        return  console.log ("Product not found");
+        return this.products
        
-    // };
+    };
+
+    async updateProduct (id, title, description, price, thumbnail, code, stock) {
+        const updateProduct = await this.getProductByid(id)
+        if (updateProduct) {
+            updateProduct.title = title;
+            updateProduct.description = description;
+            updateProduct.price = price;
+            updateProduct.thumbnail = thumbnail;
+            updateProduct.code = code;
+            updateProduct.stock = stock;
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products), 'utf8');
+            return updateProduct;
+         }
+    }
 }
 
 const productManager = new ProductManager('./products.json');
 
-const prod1 = {
-    title: 'Sprite',
-    description: 'Original',
-    price: 2800,
-    thumbnail:"https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",
-    code:1,
-    stock:30
-}
+//Prueba basada en la clase
 
-const prod2 = {
-    title: 'Fanta',
-    description: 'Edicion Loolapaloza',
-    price: 3400,
-    thumbnail:"https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",
-    code:2,
-    stock:50
-}
+// const prod1 = {
+//     title: 'Sprite',
+//     description: 'Original',
+//     price: 2800,
+//     thumbnail:"https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",
+//     code:1,
+//     stock:30
+// }
+
+// const prod2 = {
+//     title: 'Fanta',
+//     description: 'Edicion Loolapaloza',
+//     price: 3400,
+//     thumbnail:"https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",
+//     code:2,
+//     stock:50
+// }
+
+
+//Area DE pruebas
+
 
 const test = async() => {
     console.log(await productManager.getProducts())
-    // await productManager.addProduct(prod1)
-    // await productManager.addProduct(prod2)
+       
     await productManager.addProduct('Sprite', 'Original', 2800, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",1 ,30 )
+
+    await productManager.addProduct('Sprite', 'Original', 2800, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",1 ,30 ) //Codigo Duplicado
+
+    await productManager.addProduct( 'Original', 2800, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",1 ,30 ) //Campo sin completar
+
     await productManager.addProduct('Fanta', 'Edicion Loolapaloza', 3400, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",2 , 50 )
+
+    await productManager.updateProduct(2, 'Roman Riquelme', 'Edicion Loolapaloza', 3400, "https://coca-colaentucasa.com/wp-content/uploads/2024/02/3193-CCZ-473X6-CREATIONS-KWAVE.jpg",2 , 50 )
     console.log(await productManager.getProducts())
+
+    console.log(await productManager.getProductByid(2));
 }
 
 test()
@@ -122,7 +155,6 @@ test()
 
 // // Busqueda por ID
 // console.log('El producto buscado es:')
-// console.log(productManager.getProductByid(2));
 
 // Busqueda por ID fallida
 //productManager.getProductByid(6)
