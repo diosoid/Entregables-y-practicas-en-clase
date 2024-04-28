4b7e18d5646c8cd542bcf600b93a7100d017e99d
Pablo <reta_pablo_@hotmail.com>
2024-04-28 20:18:32 -0300

    Pruebas con el Update

--- Desafios entregables/Primer Desafio/src/server.js
+++ Desafios entregables/Primer Desafio/src/server.js
@@ -18,6 +18,51 @@ app.get('/products', async (req, res) => {
     }    
 })
 
+app.post('/products', async (req, res) => {
+    try {
+        const { title, description, price, thumbnail, code, stock } = req.body;
+        const product = await productManager.addProduct(title, description, price, thumbnail, code, stock);
+        res.status(201).json(product);
+        
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
+app.get ('/products/:id', async(req, res)=>{
+    try {
+        const {id} = req.params
+        const product = await productManager.getProductByid(id)
+        if(!product) res.status(404).json({msg: 'Product not found'})
+        else res.status(500).json(product)
+
+
+    } catch (error) {
+        res.status(500).json({msg: error.message})        
+    }
+})
+
+app.put ('/products/:id', async(req, res)=>{
+    try {
+        //const {id} = req.params
+        const { id } = req.params;
+        const prodUpdate = await productManager.updateProduct(id, req.body.title, req.body.description, req.body.price, req.body.thumbnail, req.body.code, req.body.stock)
+        if(!prodUpdate)res.status(404).json({msg: 'Error updating product'})
+        res.status(200).json(prodUpdate)
+
+    } catch (error) {
+        res.status(500).json({msg: error.message})        
+    }
+})
+// app.delete ('/products/:id', async(req, res)=>{
+//     try {
+//         const {id} = req.params
+
+//     } catch (error) {
+//         res.status(500).json({msg: error.message})        
+//     }
+// })
+
+
 // app.post('/products', async (req, res) => {
 //     try {
 //         const product = await productManager.addProduct(req.body)
@@ -29,16 +74,6 @@ app.get('/products', async (req, res) => {
 // })
 
 //Correcion de chat GPT a mi metodo post que no andabaS
-app.post('/products', async (req, res) => {
-    try {
-        const { title, description, price, thumbnail, code, stock } = req.body;
-        const product = await productManager.addProduct(title, description, price, thumbnail, code, stock);
-        res.status(201).json(product);
-        
-    } catch (error) {
-        res.status(500).json({ msg: error.message });
-    }
-});
 
 
 const PORT = 8080
