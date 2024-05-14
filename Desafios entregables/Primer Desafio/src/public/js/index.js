9483745b1bb5d5be9b723c5d5d2d53588683a7ea
ManuMuller <manumu99@hotmail.com>
2024-05-14 18:40:13 -0300

    Revert "Real time"
    This reverts commit d307509c9fb93686d4be658bb59b0845a87aeed4.

--- Desafios entregables/Primer Desafio/src/public/js/index.js
+++ Desafios entregables/Primer Desafio/src/public/js/index.js
@@ -1,60 +1,32 @@
 const socketClient = io()
 
-socketClient.on('saludoDesdeBack', (message) => {
+socketClient.on('saludoDesdeBack', (message)=>{
     console.log(message)
     socketClient.emit('respuestadesdefront', 'muchas gracias')
 })
 
-// const form2 = document.getElementById('form2')
-// const inputName = document.getElementById('name')
-// const inputPrice = document.getElementById('price')
-// const products = document.getElementById('products')
+const form = documnet.getElementById('form')
+const inputName = documnet.getElementById('name')
+const inputPrice = documnet.getElementById('price')
+const products = documnet.getElementById('products')
 
-// form2.onsubmit = (e) => {
-//     e.preventDefault()
-//     const name = inputName.value;
-//     const price = inputPrice.value;
-//     const product = {
-//         name,
-//         price
-//     }
-//     socketClient.emit('newProduct', product)
-// }
-
-// socketClient.on('products', (arrayProducts) => {
-//     let infoProducts = ''
-//     arrayProducts.map((prod) => {
-//         infoProducts += `${prod.name} - $${prod.price}</br>`
-//     })
-//     products.innerHTML = infoProducts
-// })
-
-const form = document.getElementById("product-form-add")
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const formData = new FormData(form)
-    const newProductData = Object.fromEntries(formData.entries())
-    socketClient.emit("newProductData", newProductData)
-})
-
-const updateTable = (products) => {
-    let bodyTable = document.getElementById('body-table')
-    bodyTable.innerHTML = ``
-    products.forEach(prod => {
-        const fila = document.createElement('tr')
-        fila.innerHTML = `  <td>${prod.title}</td>
-                            <td>${prod.description}</td>
-                            <td>${prod.price} </td>
-                            <td>${prod.code}</td>
-                            <td>${prod.stock}</td>
-                            <td>${prod.category}</td>
-                            <td>${prod.status}</td>
-                            <td>${prod.id}</td>`
-        bodyTable.appendChild(fila)
-    });
+form.onsubmit = (e) =>{
+    e.preventDefault()
+    const name = inputName.value;
+    const price = inputPrice.value;
+    const product = {
+        name,
+        price
+    }
+    socketClient.emit('newProduct', product)
 }
-socketClient.on('productsData', (products) => {
-    updateTable(products)
+
+socketClient.on('products', (arrayProducts) =>{
+    let infoProducts = ''
+    arrayProducts.map((prod) =>{
+        infoProducts += `${prod.name} - $${prod.price}</br>`
+    })
+    products.innerHTML = infoProducts
 })
 
 
--- Desafios entregables/Primer Desafio/src/server.js
+++ Desafios entregables/Primer Desafio/src/server.js
@@ -5,15 +5,13 @@ import { __dirname } from './path.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import handlebars from 'express-handlebars'
 import router from './routes/view.router.js'
-import { Server } from 'socket.io'
-import ProductManager from './manager/productManager.js';
-const productManager = new ProductManager("./src/data/products.json")
+import {Server} from 'socket.io'
 
 
 const app = express();
 app.use(express.static(__dirname + '/public'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
@@ -22,48 +20,38 @@ app.use('/api/carts', cartRouter)
 app.use('/api/products', productRouter)
 app.use('/', router)
 
-app.get('/', (req, res) => {
+app.get('/', (req,res)  =>{
     res.render('websocket')
 })
 
 app.use(errorHandler)
 
-const PORT = 3001
+const PORT = 8080
 
-const httpServer = app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
+const httpServer = app.listen(PORT, () =>console.log(`Server runing on port ${PORT}`)  )
 
 const socketServer = new Server(httpServer)
 
 const products = []
 
-socketServer.on('connection', async (socket) => {
+socketServer.on('connection', (socket)=>{
 
     console.log(`New client connected ${socket.id}`)
-    socket.on('disconnect', () => {
+    socket.on('disconnect', ()=>{
         console.log(`Client disconnected ${socket.id}`)
     })
 
-    // socket.emit('saludoDesdeBack', 'Bienvenido a web socket')
+    socket.emit('saludoDesdeBack', 'Bienvenido a web socket')
 
-    // socket.on('respuestadesdefront', (message) => {
-    //     console.log(message)
-    // })
-
-    // socket.on('newProduct', (prod) => {
-    //     products.push(prod)
-    //     socket.emit('products', products)
-    // })
-    const productsData = await productManager.getProducts()
-    socket.emit("productsData", productsData)
-    socket.on("newProductData", async (prod) => {
-        await productManager.addProduct(prod.title,
-            prod.description,
-            prod.price,
-            prod.thumbnails,
-            prod.code,
-            prod.stock,
-            prod.category)
+    socket.on('respuestadesdefront', (message) =>{
+        console.log(message)     
     })
+
+    socket.on('newProduct', (prod) =>{
+        products.push(prod)
+        socketServer.emit('products', products)
+    })
+    
 })
 
 
