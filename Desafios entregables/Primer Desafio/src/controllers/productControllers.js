1461950f44957a802e27c35c411e7cca6802fd86
Pablo <reta_pablo_@hotmail.com>
2024-06-16 20:46:37 -0300

    .

--- Desafios entregables/Primer Desafio/src/controllers/productControllers.js
+++ Desafios entregables/Primer Desafio/src/controllers/productControllers.js
@@ -22,41 +22,41 @@ export const getAllProducts = async (req, res, next) =>{
         })
         res.status(200).json(response)       
     } catch (error) {
-        next (error)      
+        next (error.message)      
     }}
 export const getProductByid = async (req, res, next) =>{
     try {
-        const {id} = await req.params //Aca no puso el await no se porque
+        const {id} =  req.params //Saque el await aca
         const product = await service.getById (id)
         if(!product) res.status(404).json ({msg: 'product not found'})
         else res.json(product)       
     } catch (error) {
-        next (error)      
+        next (error.message)      
     }}
 export const createProduct = async (req, res, next) =>{
     try {
-        const newProd = await service.create(req.body)
+        const newProd = await service.create(req.body) 
         if(!newProd) res.status(404).json ({msg: 'Error creating product'})
         else res.json(newProd)      
     } catch (error) {
-        next (error)      
+        next (error.message)      
     }}
 export const updateProduct = async (req, res, next) =>{
     try {
-        const {id} = await req.params //Aca tampoco puso await
+        const {id} =  req.params //Saque el await aca
         const prodUpd = await service.update(id, req.body)
         if(!prodUpd) res.status(405).json ({msg: 'Error updating product'})
         else res.json(prodUpd)
     } catch (error) {
-        next (error)      
+        next (error.message)      
     }}
 
     export const remove = async (req, res, next) =>{
         try {
-            const {id} = await req.params // Tampoco uso el await
+            const {id} = await req.params 
             const prodDel = await service.remove(id)
             if(!prodDel) res.status(404).json ({msg: 'Error deleting product'})
             else res.json(prodDel)       
         } catch (error) {
-            next (error)      
+            next (error.message)      
         }}
